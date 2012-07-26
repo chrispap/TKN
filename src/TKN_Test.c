8efa59bf861f814cc7e8089df94486533f249c4e
Chris Papapavlou <chrispapapaulou@gmail.com>
2012-07-26 03:36:22 +0300

    Fix Indentation

--- src/TKN_Test.c
+++ src/TKN_Test.c
@@ -12,24 +12,24 @@ static BYTE TOKEN_PACKET[TKN_OFFS_TOKEN_EOF + 1]  = { 0x00, TKN_TYPE_TOKEN, 0x00
 int main(int argc, char **argv)
 {
     int i,size;
-	FILE *fd = NULL;
-	char packetPath[50] = "packets/";
+    FILE *fd = NULL;
+    char packetPath[50] = "packets/";
     char *str_ptr = packetPath + strlen(packetPath);
 
-	strcpy(str_ptr, "DATA.hex");
+    strcpy(str_ptr, "DATA.hex");
     fd = fopen(packetPath, "w");
     size = sizeof(DATA_PACKET);
     for (i=0; i<size; i++) fputc(DATA_PACKET[i], fd);
     
-	strcpy(str_ptr, "ACK.hex");
+    strcpy(str_ptr, "ACK.hex");
     fd = fopen(packetPath, "w");
     size = sizeof(ACK_PACKET);
     for (i=0; i<size; i++) fputc(ACK_PACKET[i], fd);
     
-	strcpy(str_ptr, "TOKEN.hex");
+    strcpy(str_ptr, "TOKEN.hex");
     fd = fopen(packetPath, "w");
     size = sizeof(TOKEN_PACKET);
     for (i=0; i<size; i++) fputc(TOKEN_PACKET[i], fd);
     
-	return 0;
+    return 0;
 }
