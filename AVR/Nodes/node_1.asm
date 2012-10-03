f8f4ffb96618ad6bb9847c04572354cfa428013f
Chris Papapavlou <chrispapapaulou@gmail.com>
2012-10-03 23:23:07 +0300

    Test AVR code with timer0

--- AVR/Nodes/node_1.asm
+++ AVR/Nodes/node_1.asm
@@ -14,37 +14,52 @@
 .cseg
 ;Vectors
 .org 0x0000
-	jmp main
+    jmp main
 
 .dseg
 .org $400
 packetBuff:	.byte TKN_PACKET_SIZE
 
 .cseg
-str:	.db "                "
+str:	.db "Timer counted!  "
 
 main:
-
     ldi YL, LOW(packetBuff)
-	ldi YH, HIGH(packetBuff)
-
-	ldi ZL, LOW(str<<1)
-	ldi ZH, HIGH(str<<1)
-
-	call fillPacketBuf
-
-	mov ZL, YL
-	mov ZH, YH
-	ldi counter, 5
-
+    ldi YH, HIGH(packetBuff)
+    ldi ZL, LOW(str << 1)
+    ldi ZH, HIGH(str << 1)
+    call fillPacketBuf
+    
+    ldi counter, $4
+    
+    clr temp0
+    out TCNT0, temp0
+    
+    clr temp0
+    out TCCR0A, temp0
+    ldi temp0, 0x05
+    out TCCR0B, temp0
+    
 loop:
-	mov temp0, counter
-	call bin1ToHex2
-
-	ldi temp0, 1
-	call TKN_pushPacket
-
-	dec counter
-	brne loop
-	
-	ret
+    ldi temp1, $FF
+    ldi temp2, $3
+lp:
+    in temp0, TCNT0
+    cpi temp0, $FF
+    brne lp
+    dec temp1
+    brne lp
+    dec temp2
+    brne lp
+    
+    ldi temp0, 1
+    call TKN_pushPacket
+    
+    dec counter
+    brne loop
+    
+    clr temp0
+    out TCCR0A ,temp0
+    out TCCR0B ,temp0
+    
+    ret
